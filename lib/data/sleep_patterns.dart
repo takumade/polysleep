0fdc5e6d669882036e859dd3c4ed0ac6419be9bc
Takunda Madechangu <madechangu.takunda@gmail.com>
2024-10-27 12:48:57 +0200

    Add sleep pattern data

---
+++ lib/data/sleep_patterns.dart
@@ -0,0 +1,36 @@
+
+
+import 'package:polysleep/models/sleep_patterns.dart';
+
+List<SleepPattern> patterns = [
+  SleepPattern(
+    id: "monophasic",
+    name: "Monophasic sleep",
+    description: "Monophasic sleep is a type of sleep that is taken in one single block of time. Most people in Western societies sleep in a monophasic pattern. This means that they sleep for one block of time at night.",
+    imageUrl: ""
+  ), 
+  SleepPattern(
+    id: "biphasic",
+    name: "Biphasic sleep",
+    description: "Biphasic sleep is a type of sleep pattern where a person sleeps for two blocks of time each day. This can be in the form of a long sleep at night and a short nap during the day, or two equal-length sleeps separated by a period of wakefulness.",
+    imageUrl: ""
+  ),
+  SleepPattern(
+    id: "everyman",
+    name: "Everyman sleep",
+    description: "Everyman sleep is a type of polyphasic sleep pattern that consists of a longer core sleep period and several short naps throughout the day. The core sleep period is usually around 3-4 hours long, and the naps are around 20-30 minutes each.",
+    imageUrl: ""
+  ),
+  SleepPattern(
+    id: "uberman",
+    name: "Uberman sleep",
+    description: "Uberman sleep is a type of polyphasic sleep pattern that consists of six 20-minute naps taken throughout the day. This sleep pattern is one of the most extreme forms of polyphasic sleep and is very difficult to maintain.",
+    imageUrl: ""
+  ),
+  SleepPattern(
+    id: "dymaxion",
+    name: "Dymaxion sleep",
+    description: "Dymaxion sleep is a type of polyphasic sleep pattern that consists of four 30-minute naps taken throughout the day. This sleep pattern was popularized by inventor Buckminster Fuller, who claimed that it allowed him to be more productive and creative.",
+    imageUrl: ""
+  )
+];

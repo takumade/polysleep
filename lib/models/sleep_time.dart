049188c615e58f9d6a535cc0769486e8542a2fc4
Takunda Madechangu <madechangu.takunda@gmail.com>
2024-11-06 06:16:33 +0200

    modify sleep time model

--- lib/models/sleep_time.dart
+++ lib/models/sleep_time.dart
@@ -2,21 +2,25 @@ class SleepTime {
   final String id;
   final String name;
   final String difficulty;
+  final String proposer = "puredoxyk";
   final List<SleepType> sleepTypes;
   List<SleepVariants> sleepVariants = [];
 
-  SleepTime({required this.id, required this.name, required this.difficulty, required this.sleepTypes});
+  SleepTime({required this.id, required this.name, required this.difficulty, required this.sleepTypes, required this.sleepVariants});
 
 }
 
 class SleepVariants {
+  final String name;
   List<SleepType> sleepTypes = [];
+
+  SleepVariants({required this.name, required this.sleepTypes});
 }
 
 class  SleepType {
   final String name;
-  final DateTime startTime;
-  final DateTime endTime;
+  final String startTime;
+  final String endTime;
   final SleepTypeEnum type;
 
   SleepType({required this.name, required this.startTime, required this.endTime, required this.type});
